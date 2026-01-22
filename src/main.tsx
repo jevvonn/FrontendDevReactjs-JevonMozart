501a5126ca3c87a04cf6764b32b5704b637e1c39
jevvonn <darmawanb985@gmail.com>
2026-01-22 20:03:13 +0700

    feat: add react-router for routing and update main component to use RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>,
 );
